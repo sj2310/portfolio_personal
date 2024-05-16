3dbdcf19b053648618a0f32f7d5a54c99fc78d57
Sajal Jain <sajaljn23@gmail.com>
2024-05-16 22:23:24 +0530

    Updated CSS styles and removed mediaquery.css file

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,4 @@
 import "./css/navbar.css";
-import "./css/mediaquery.css";
 import { useState } from "react";
 
 export const Navbar = () => {
@@ -12,37 +11,20 @@ export const Navbar = () => {
     <div className="navbar">
       <div className="logo">Sajal Jain</div>
       <div className="menu">
-        <div
-          className={`navbar-menu-icon ${open ? "change" : ""}`}
-          onClick={toggleMenu}
-        >
+        <div className={`navbar-menu-icon ${open ? "change" : ""}`} onClick={toggleMenu}>
           <div className="box"></div>
           <div className="box"></div>
           <div className="box"></div>
           <div className="box"></div>
         </div>
-        <ul className={`navbar-menu ${open ? "animate" : ""}`}>
-          <li>
-            <a href="#aboutId" onClick={toggleMenu}>
-              About
-            </a>
-          </li>
-          <li>
-            <a href="#skillId" onClick={toggleMenu}>
-              Skills
-            </a>
-          </li>
-          <li>
-            <a href="#projectId" onClick={toggleMenu}>
-              Projects
-            </a>
-          </li>
-          <li>
-            <a href="#contactId" onClick={toggleMenu}>
-              Contact
-            </a>
-          </li>
-        </ul>
+        {/* <div> */}
+          <ul className={`navbar-menu ${open?"animate":""}`}>
+           <li><a href="#aboutId" onClick={toggleMenu}>About</a></li>
+           <li><a href="#projectId" onClick={toggleMenu}>Projects</a></li>
+           <li><a href="#contactId" onClick={toggleMenu}>Contact</a></li>
+           <li><a href="#skillId" onClick={toggleMenu}>Skills</a></li>
+          </ul>
+        {/* </div> */}
       </div>
       <div className="menu2">
       <ul className="menu2-list">
